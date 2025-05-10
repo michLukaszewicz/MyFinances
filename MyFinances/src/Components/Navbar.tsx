47123bd3581b2a33fb0f2b2ff12f01b6e2698191
Michal Lukaszewicz <michal.lukaszewicz@precast.pl>
2025-05-10 13:36:29 +0200

    implemented default navbar

--- MyFinances/src/Components/Navbar.tsx
+++ MyFinances/src/Components/Navbar.tsx
@@ -1,5 +1,6 @@
 import "./Navbar.css";
 import logo from "../assets/logo.png";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 interface Props {}
 
@@ -14,20 +15,32 @@ const Navbar = (props: Props) => {
       <a href="/">
         <img src={logo} alt="logo" className="w-32 h-8" />
       </a>
-      <ul className="hidden md:flex gap-4 text-md font-semibold text-gray-700">
-        <li className="">
-          <a>Home</a>
+      <ul className="hidden md:flex gap-7 font-semibold text-gray-700 mx-auto">
+        <li className="btn">
+          <a className="active">Home</a>
         </li>
-        <li className="">
-          <a>Summary</a>
+        <li className="btn">
+          <a >Summary</a>
         </li>
-        <li className="">
+        <li className="btn">
           <a>Finances</a>
         </li>
-        <li className="">
+        <li className="btn">
           <a>Accounts</a>
         </li>
       </ul>
+
+      <ul className="hidden md:flex gap-5 font-semibold text-gray-700">
+        <li className="btn">
+          <a>Login</a>
+        </li>
+        <li className="btn">
+          <a>Register</a>
+        </li>
+      </ul>
+      <div className="md:hidden flex items-center hover:text-blue-600 cursor-pointer">
+        <GiHamburgerMenu />
+      </div>
     </header>
   );
 };
