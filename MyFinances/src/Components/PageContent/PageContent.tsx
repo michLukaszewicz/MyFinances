e55ec750182bf8da22921121fd17adbedf9d3b71
Michal Lukaszewicz <michal.lukaszewicz@precast.pl>
2025-05-11 16:11:27 +0200

    updated width of layout

--- MyFinances/src/Components/PageContent/PageContent.tsx
+++ MyFinances/src/Components/PageContent/PageContent.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 const PageContent = ({ children }: Props) => {
   return (
-    <main className="py-8 max-w-7x1 mx-auto px-4 sm:px-6 lg:px-8 w-full">
+    <main className="py-8 mx-auto px-4 sm:px-6 lg:px-8 w-full">
       {children}
     </main>
   );
