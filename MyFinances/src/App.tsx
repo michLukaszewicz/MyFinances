ba7e05296008b4e9fac4a498843ad7e357b9df5f
Michal Lukaszewicz <michal.lukaszewicz@precast.pl>
2025-05-11 16:25:24 +0200

    move reading TransactionHistory.json to App()

--- MyFinances/src/App.tsx
+++ MyFinances/src/App.tsx
@@ -2,13 +2,21 @@ import "./App.css";
 import HistoryPanel from "./Components/HistoryPanel/HistoryPanel";
 import Navbar from "./Components/Navbar/Navbar";
 import PageContent from "./Components/PageContent/PageContent";
+import type { Transaction } from "./Models/Transaction";
+import transactionHistory from "./TestData/TransactionHistory.json";
+
+const history: Transaction[] = transactionHistory.map((item) => ({
+  ...item,
+  date: new Date(item.date),
+  type: item.amount > 0 ? "income" : "expense",
+}));
 
 function App() {
   return (
     <div className="w-full h-full">
       <Navbar />
       <PageContent>
-        <HistoryPanel />
+        <HistoryPanel history={history} />
       </PageContent>
     </div>
   );
--- MyFinances/src/Components/HistoryPanel/HistoryPanel.tsx
+++ MyFinances/src/Components/HistoryPanel/HistoryPanel.tsx
@@ -1,21 +1,20 @@
 import type { Transaction } from "../../Models/Transaction";
-import transactionHistory from "../../TestData/TransactionHistory.json";
 import Box from "../Box/Box";
 import "./HistoryPanel.css";
 import ListRow from "./ListRow/ListRow";
 
-const history: Transaction[] = transactionHistory.map((item) => ({
-  ...item,
-  date: new Date(item.date),
-  type: item.amount > 0 ? "income" : "expense",
-}));
+interface Props {
+  history: Transaction[];
+}
 
-const HistoryPanel = () => {
+const HistoryPanel = ({history}: Props) => {
   return (
     <Box>
       <div className="flex justify-between mb-3">
         <h1 className="font-bold text-2xl">History Panel</h1>
-        <a href="#" className="text-gray-500">View All &gt;</a>
+        <a href="#" className="text-gray-500">
+          View All &gt;
+        </a>
       </div>
       <div className="flex flex-col justify-between">
         {history.map((transaction) => (
