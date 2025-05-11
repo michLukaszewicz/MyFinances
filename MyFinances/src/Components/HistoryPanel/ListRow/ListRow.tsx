e5f877af1f0d1650eb1ab3effafadcbd1cdd9695
Michal Lukaszewicz <michal.lukaszewicz@precast.pl>
2025-05-11 15:54:21 +0200

    add formating of amout in ListRow

--- MyFinances/src/Components/HistoryPanel/ListRow/ListRow.tsx
+++ MyFinances/src/Components/HistoryPanel/ListRow/ListRow.tsx
@@ -6,21 +6,33 @@ type Props = {
 
 const ListRow = ({ transaction }: Props) => {
   return (
-    <div className="grid [grid-template-columns:3fr_1.5fr_1fr] gap-4 mx-2 my-2 items-center border-b border-gray-200 pb-2">
+    <div className="grid [grid-template-columns:2.5fr_1fr_1fr] gap-4 mx-2 my-2 items-center border-b border-gray-200 pb-2">
       <div>
-        <p className="font-medium text-blue-700 text-lg">
+        <p className="text-blue-700 text-md">
           {transaction.otherAccount}
         </p>
         <p className="text-xs text-gray-500">{transaction.description}</p>
       </div>
       <div className="text-center">
-        <p className="font-medium text-blue-700">
-          {transaction.date.toLocaleDateString("pl-PL", {day: '2-digit', month: '2-digit'})}
+        <p className="text-blue-700 text-sm">
+          {transaction.date.toLocaleDateString("pl-PL", {
+            day: "2-digit",
+            month: "2-digit",
+          })}
         </p>
         <p className="text-xs text-gray-500">{transaction.account}</p>
       </div>
       <div className="text-right">
-        <p className="font-extrabold text-xl">{transaction.amount}</p>
+        <p
+          className={`font-bold text-lg ${
+            transaction.amount > 0 ? "text-green-600" : "text-red-700"
+          }`}
+        >
+          {transaction.amount.toLocaleString("pl-PL", {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+          })}
+        </p>
         <p className="text-[11px] text-gray-500">PLN</p>
       </div>
     </div>
