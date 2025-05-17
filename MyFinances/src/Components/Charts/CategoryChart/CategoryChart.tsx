a72c831dba746ca12c51c0059fbe6874d2d95c07
Michal Lukaszewicz <michal.lukaszewicz@precast.pl>
2025-05-17 13:30:13 +0200

    updated charts in HomePage

--- MyFinances/src/Components/Charts/CategoryChart/CategoryChart.tsx
+++ MyFinances/src/Components/Charts/CategoryChart/CategoryChart.tsx
@@ -9,21 +9,30 @@ type Props = {
 const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "#FF00FF", "#FF4500", "#FFD700", "#ADFF2F", "#7FFF00", "#00FFFF"];
 
 const CategoryChart = ({ history }: Props) => {
-  const total = history.reduce((acc, transaction) => acc + Math.abs(transaction.amount), 0);
+  const total = history.reduce((acc, transaction) => {
+    if (transaction.amount < 0) {
+      acc += Math.abs(transaction.amount);
+    }
+    return acc;
+  }, 0);
+
   const totalByCategory = history.reduce(
     (acc, transaction) => {
-      const category = transaction.category;
-      if (!acc[category]) {
-        acc[category] = 0;
+      if (transaction.amount < 0) {
+        const category = transaction.category;
+        if (!acc[category]) {
+          acc[category] = 0;
+        }
+        acc[category] += Math.abs(transaction.amount);
       }
-      acc[category] += Math.abs(transaction.amount);
       return acc;
     },
     {} as Record<string, number>
   );
+
   const percentageByCategory: Record<string, string> = Object.entries(totalByCategory).reduce(
-    (acc, [category, categoryValue]) => {
-      const percent = ((categoryValue / total) * 100).toFixed(1); // np. 66.7
+    (acc: Record<string, string>, [category, value]) => {
+      const percent = ((value / total) * 100).toFixed(1);
       acc[category] = `${percent}%`;
       return acc;
     },
@@ -32,17 +41,17 @@ const CategoryChart = ({ history }: Props) => {
 
   const data = Object.entries(totalByCategory).map(([category, value]) => ({
     name: category,
-    value: value,
+    value,
     label: `${category} (${percentageByCategory[category]})`,
   }));
 
   return (
     <Box>
       <div className="flex justify-between mb-3">
-        <p className="font-bold text-2xl mb-3">Category Chart</p>
+        <p className="font-bold text-2xl mb-3">Expenses by Category</p>
         <div>
-          {/* dodać tutaj możliwość wyłączeina wybranych kategorii i wybranie okresu  */}
-          <p className="text-blue-600 text-sm text-right">Settings</p>
+          {/* tutaj można dodać ustawienia filtrowania */}
+          <p className="text-blue-600 text-sm text-right cursor-pointer">Settings</p>
         </div>
       </div>
       <div className="flex flex-col items-center">
