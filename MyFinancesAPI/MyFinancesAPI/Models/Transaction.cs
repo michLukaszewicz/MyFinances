e63715ca4158e372e6444f19252f0ca710c40543
Michal Lukaszewicz <michal.lukaszewicz@precast.pl>
2025-05-18 12:57:24 +0200

    Implemented TestDataController in API

---
+++ MyFinancesAPI/MyFinancesAPI/Models/Transaction.cs
@@ -0,0 +1,13 @@
+﻿namespace MyFinancesAPI.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+        public string Description { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+        public string Category { get; set; } = string.Empty;
+        public string BankAccount { get; set; } = string.Empty;
+        public string OtherSideOfTransaction { get; set; } = string.Empty;
+    }
+}
